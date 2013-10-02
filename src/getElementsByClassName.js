05de9886d03a08c58f238fb09ad1fa073785b4aa
Wayne Montague <wayne.montague@gmail.com>
2013-10-02 09:38:21 -0700

    the getElementsByClassName function is passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,31 @@
 // };
 
 // But in stead we're going to implement it from scratch:
+
+var walkTheDOM = function(node, func) {
+  var children = node.childNodes;
+
+  func(node);
+
+  for (var i = 0; i < children.length; i++) {
+    walkTheDOM(children[i], func);
+  }
+};
+
 var getElementsByClassName = function (className) {
-  // your code here
+  var results = [];
+
+  if (typeof className !== 'string') {
+    throw new TypeError('className must be a string');
+  }
+  walkTheDOM(document.body, function(node) {
+    if (node.nodeType === 1) {
+      for (var i = 0; i < node.classList.length; i++) {
+        if (node.classList[i] === className) {
+          results.push(node);
+        }
+      }
+    }
+  });
+  return results;
 };
