5682fd129bd2a07b6e933590b2d36eac7227080d
Wayne Montague <wayne.montague@gmail.com>
2013-10-04 10:06:36 -0700

    all but one test is passing. I need to return an empty string when I encounter and undefined value.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,18 +4,23 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
   var rslt = [];
+  var type = Object.prototype.toString.call(obj).slice(8, -1);
 
-  if (Array.isArray(obj)) {
+  if (type === "Array") {
     for (var i = 0; i < obj.length; i++) {
       rslt.push(stringifyJSON(obj[i]));
     }
     return "[" + rslt.join(",") + "]";
-  } else if (Object.prototype.toString.call(obj).slice(8, -1) === "Object") {
+  } else if (type === "Object") {
     for (var prop in obj) {
-      rslt.push(prop.toString() + ": " + stringifyJSON(obj[prop]));
+      rslt.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
     }
     return "{" + rslt.join(",") + "}";
+  } else if (type === "String") {
+    return '"' + obj + '"';
   } else {
-    return String(obj);
+    if (typeof obj !== "function") {
+      return String(obj);
+    }
   }
 };
