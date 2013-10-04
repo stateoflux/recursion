2f0628a5e2364c430e73df51969cc52d58e78bb6
Wayne Montague <wayne.montague@gmail.com>
2013-10-04 09:46:32 -0700

    This implementation sort of works, looks like I just need to surround strings with double quotes.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,19 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var rslt = [];
+
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i++) {
+      rslt.push(stringifyJSON(obj[i]));
+    }
+    return "[" + rslt.join(",") + "]";
+  } else if (Object.prototype.toString.call(obj).slice(8, -1) === "Object") {
+    for (var prop in obj) {
+      rslt.push(prop.toString() + ": " + stringifyJSON(obj[prop]));
+    }
+    return "{" + rslt.join(",") + "}";
+  } else {
+    return String(obj);
+  }
 };
