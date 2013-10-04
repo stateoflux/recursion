dfd836d3e02b91f802069cb45cf6a425d9f90f8b
Wayne Montague <wayne.montague@gmail.com>
2013-10-04 12:02:25 -0700

    stringifyJSON function is passing all of it's tests.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,21 +5,27 @@
 var stringifyJSON = function (obj) {
   var rslt = [];
   var type = Object.prototype.toString.call(obj).slice(8, -1);
+  var isFuncOrUndefined = function(obj) {
+    return typeof obj === "undefined" || typeof obj === "function";
+  };
 
   if (type === "Array") {
     for (var i = 0; i < obj.length; i++) {
-      rslt.push(stringifyJSON(obj[i]));
+      if (!isFuncOrUndefined(obj[i]))
+        rslt.push(stringifyJSON(obj[i]));
     }
     return "[" + rslt.join(",") + "]";
   } else if (type === "Object") {
     for (var prop in obj) {
-      rslt.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
+      if (!isFuncOrUndefined(obj[prop])) {
+        rslt.push(stringifyJSON(prop) + ":" + stringifyJSON(obj[prop]));
+      }
     }
     return "{" + rslt.join(",") + "}";
   } else if (type === "String") {
     return '"' + obj + '"';
   } else {
-    if (typeof obj !== "function") {
+    if (!isFuncOrUndefined(obj)) {
       return String(obj);
     }
   }
